c0a13f08aba2c0803c307780b43effef7063f07f
Col0ring <1561999073@qq.com>
2021-11-10 00:57:06 +0800

    chore: add env

---
+++ stylelint.config.js
@@ -0,0 +1,38 @@
+const prettierConfig = require('./prettier.config')
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-rational-order',
+    'stylelint-prettier/recommended',
+  ],
+  plugins: [
+    'stylelint-declaration-block-no-ignored-properties',
+    'stylelint-less',
+  ],
+  rules: {
+    'no-descending-specificity': null,
+    'plugin/declaration-block-no-ignored-properties': true,
+    'prettier/prettier': [true, prettierConfig],
+    'no-invalid-position-at-import-rule': null,
+    'at-rule-no-unknown': [true, { ignoreAtRules: 'plugin' }],
+    'rule-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested'],
+      },
+    ],
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
+  },
+  ignoreFiles: [
+    '**/node_modules/**/*',
+    '**/es/**/*',
+    '**/lib/**/*',
+    '**/docs-dist/**/*',
+    '**/.umi/**/*',
+  ],
+}
